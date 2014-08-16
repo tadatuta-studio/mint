0542b2fca183b044316d655e9f9509fa7855bae9
Ivan Avraamov <avraamov@tadatuta.com>
2014-08-16 09:18:27 +0400

    top slider adding

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -96,6 +96,50 @@
             block: 'first-screen',
             content: [
                 {
+                    block: 'carousel',
+                    mix: { block: 'first-screen', elem: 'carousel' },
+                    mods: { animate: 'yes' },
+                    content: [
+                        {
+                            elem: 'inner',
+                            content: [
+                                {
+                                    elem: 'item',
+                                    elemMods: { state: 'active' },
+                                    content: {
+                                        elem: 'img',
+                                        url: '/i/first-screen-slider_one.png'
+                                    }
+                                },
+                                {
+                                    elem: 'item',
+                                    content: {
+                                        elem: 'img',
+                                        url: '/i/first-screen-slider_two.png'
+                                    }
+                                },
+                                {
+                                    elem: 'item',
+                                    content: {
+                                        elem: 'img',
+                                        url: '/i/first-screen-slider_three.png'
+                                    }
+                                }
+                            ]
+                        }
+                        // {
+                        //     elem: 'control',
+                        //     elemMods: { type: 'left', theme: 'mini' },
+                        //     content: ''
+                        // },
+                        // {
+                        //     elem: 'control',
+                        //     elemMods: { type: 'right', theme: 'mini' },
+                        //     content: ''
+                        // }
+                    ]
+                },
+                {
                     block: 'sidebar',
                     content: [
                         {
@@ -141,50 +185,6 @@
                         // }
                     ]
                 }
-                // {
-                //     block: 'carousel',
-                //     mix: { block: 'first-screen', elem: 'carousel' },
-                //     mods: { animate: 'yes' },
-                //     content: [
-                //         {
-                //             elem: 'inner',
-                //             content: [
-                //                 {
-                //                     elem: 'item',
-                //                     elemMods: { state: 'active' },
-                //                     content: {
-                //                         elem: 'img',
-                //                         url: 'https://lh5.ggpht.com/sxYdNHOqblPwcd3fCR1sj5gzlib4sy2sl1btHdwA28WVPDxyj6hpXoOT-a4fz4nzKwnX'
-                //                     }
-                //                 },
-                //                 {
-                //                     elem: 'item',
-                //                     content: {
-                //                         elem: 'img',
-                //                         url: 'https://lh5.ggpht.com/s7830ZGWwOtA2CTApJk0ZvWny7zIbuXaOe5GlqPkknGktUmh9LBM0ojT-tjy-0dp1gM'
-                //                     }
-                //                 },
-                //                 {
-                //                     elem: 'item',
-                //                     content: {
-                //                         elem: 'img',
-                //                         url: 'https://lh6.ggpht.com/Ar_0ImGRpXy7F1QlXWQmsT6IjKPzqC8uQ9n8JgefpZzZsUK_Xpz2sL8SyFGwVJR37YTH'
-                //                     }
-                //                 }
-                //             ]
-                //         },
-                //         {
-                //             elem: 'control',
-                //             elemMods: { type: 'left', theme: 'default' },
-                //             content: '&lsaquo;'
-                //         },
-                //         {
-                //             elem: 'control',
-                //             elemMods: { type: 'right', theme: 'default' },
-                //             content: '&rsaquo;'
-                //         }
-                //     ]
-                // }
             ]
         },
         {
