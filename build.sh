4e581b4ceaf9c71e063adaa060a33a1ea0fff45c
Vladimir Grinenko <i@tadatuta.com>
2014-11-04 01:04:05 +0300

    next

---
+++ build.sh
@@ -0,0 +1,11 @@
+OUTPUT_DIR=output
+
+YENV=production ./node_modules/.bin/bem make
+mkdir -p $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR/i
+cp desktop.bundles/index/index.html $OUTPUT_DIR/
+cp desktop.bundles/index/_index.css $OUTPUT_DIR/
+cp desktop.bundles/index/_index.ie8.css $OUTPUT_DIR/
+cp desktop.bundles/index/_index.js $OUTPUT_DIR/
+cp -r desktop.bundles/index/i/* $OUTPUT_DIR/i/
+cp -r i/* $OUTPUT_DIR/i/
--- desktop.blocks/timer/timer.js
+++ desktop.blocks/timer/timer.js
@@ -1,37 +1,31 @@
-modules.define('i-bem__dom', function(provide, BEMDOM) {
+modules.define('timer', ['i-bem__dom'], function(provide, BEMDOM) {
 
-BEMDOM.decl( 'timer',
-    {
-        onSetMod: {
-            'js': {
-                inited : function(){
-                    var curDate,
-                        date = this.params.time,
-                        dateDiff,
-                        days = this.findElem('days'),
-                        hours = this.findElem('hours'),
-                        minutes = this.findElem('minutes'),
-                        timerChange = function(){
-                            curDate = new Date();
-                            dateDiff = new Date(date - curDate.getTime())
-                            days.html(dateDiff.getDate()< 10 ? '0' + dateDiff.getDate(): dateDiff.getDate());
-                            hours.html(dateDiff.getHours()< 10 ? '0' + dateDiff.getHours(): dateDiff.getHours());
-                            minutes.html(dateDiff.getMinutes() < 10 ? '0' + dateDiff.getMinutes(): dateDiff.getMinutes());
-                        };
+provide(BEMDOM.decl('timer', {
+    onSetMod: {
+        js: {
+            inited : function(){
+                var curDate,
+                    date = this.params.time,
+                    dateDiff,
+                    days = this.findElem('days'),
+                    hours = this.findElem('hours'),
+                    minutes = this.findElem('minutes'),
+                    timerChange = function(){
+                        curDate = new Date();
+                        dateDiff = new Date(date - curDate.getTime())
+                        days.html(dateDiff.getDate()< 10 ? '0' + dateDiff.getDate(): dateDiff.getDate());
+                        hours.html(dateDiff.getHours()< 10 ? '0' + dateDiff.getHours(): dateDiff.getHours());
+                        minutes.html(dateDiff.getMinutes() < 10 ? '0' + dateDiff.getMinutes(): dateDiff.getMinutes());
+                    };
+
+                timerChange();
+
+                setInterval(function() {
                     timerChange();
-                    var setTimer = setInterval (function()
-                        {
-                            timerChange()
-                        },60 * 1000);
-                }
+                }, 60 * 1000);
             }
         }
-    },
-    {
-        /* статические методы */
     }
-);
-
-provide(BEMDOM);
+}));
 
 });
